a320ccf2dc5d8789d3b0060125c83ef7e037fc32
KFerreira1221 <160679316+KFerreira1221@users.noreply.github.com>
2026-02-17 15:40:47 -0500

    Update schema.py

---
+++ mlops-claims/src/schema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Dict, Any
+
+class PredictRequest(BaseModel):
+    features: Dict[str, Any]
+
+class PredictResponse(BaseModel):
+    prediction: float
+    model_version: str
