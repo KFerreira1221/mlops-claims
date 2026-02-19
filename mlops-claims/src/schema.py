6524a4ee05e253b01a9db0bb4ab4b4ae69e98ffa
KFerreira1221 <160679316+KFerreira1221@users.noreply.github.com>
2026-02-19 11:47:20 -0500

    Delete mlops-claims directory

--- mlops-claims/src/schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict, Any
-
-class PredictRequest(BaseModel):
-    features: Dict[str, Any]
-
-class PredictResponse(BaseModel):
-    prediction: float
-    model_version: str
